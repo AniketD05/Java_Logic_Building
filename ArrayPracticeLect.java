b5e87a2acb61ca99d8c7515679074d29c7832d40
Aniket Dhiwar <aniketdhiwar1999@gmail.com>
2024-03-01 10:38:51 +0530

    Array variations

--- ArrayPracticeLect.java
+++ ArrayPracticeLect.java
@@ -9,26 +9,44 @@ public class ArrayPracticeLect {
         // System.out.println(a[1]);
 
         // for (int i = 0; i < a.length; i++) {
-        // System.out.println("Number: " + a[i] + " is present in given Array at Index:
-        // " + i);
+        // System.out.println("Number: " + a[i] + " is present in given Array at Index: " + i);
 
         // }
         // int c[] = { 12, 34, 56, 78, 'a' };
         // for (int i = 0; i < c.length; i++)
-        // System.out.println("Number: " + c[i] + " is present in given Array at Index:
-        // " + i);
+        // System.out.println("Number: " + c[i] + " is present in given Array at Index: " + i);
         // }
 
         // for (int i : a) {
         // System.out.println(i);
         // }
 
+        // Scanner inp = new Scanner(System.in);
+
+        // System.out.print("Enter your name: ");
+        // String name = inp.nextLine();
+
+        // System.out.println(name);
+
+        // inp.close();
+
         Scanner inp = new Scanner(System.in);
 
-        System.out.print("Enter your name: ");
-        String name = inp.nextLine();
+        int arr[][] = new int[2][2];
 
-        System.out.println(name);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print("Enter number you wish to add at index " + i + " " + j + " --> ");
+                arr[i][j] = inp.nextInt();
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
 
         inp.close();
 
