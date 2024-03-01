eeb8c6dcbf524b2ed7191526ba48258bf21dd24e
Aniket Dhiwar <aniketdhiwar1999@gmail.com>
2024-03-01 17:06:15 +0530

    Pattern Practice

---
+++ PatternPractice.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PatternPractice {
+    public static void main(String args[]) {
+
+        // Pattern-1
+        // *
+        // * *
+        // * * *
+        // * * * * *
+
+        // Scanner inp = new Scanner(System.in);
+
+        // System.out.println("Enter Number of Rows: ");
+        // int num = inp.nextInt();
+        // for (int i = 1; i <= num; i++) {
+
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(" * ");
+        // }
+        // System.out.println();
+        // }
+        // inp.close();
+
+       
+    }
+}
