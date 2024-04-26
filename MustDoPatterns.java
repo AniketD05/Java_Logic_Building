d7112431914600b13ddf135b9dfc40e7a2cb1713
AniketD05 <aniketdhiwar1999@gmail.com>
2024-04-26 10:53:00 +0530

    file update

--- MustDoPatterns.java
+++ MustDoPatterns.java
@@ -111,16 +111,40 @@ public class MustDoPatterns {
 
     public void pattern7(int num) {
 
-        
-        //         *
-        //       * * *
-        //     * * * * *
-        //   * * * * * * *
-        // * * * * * * * * *
-         
-         
+        // * its star pyramid
 
         for (int i = 1; i <= num; i++) {
+
+            for (int j = 1; j <= num - i + 1; j++) {
+                System.out.print("   ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print(" * ");
+            }
+
+            System.out.println();
+
+        }
+    }
+
+    public void pattern8(int num) {
+
+        // * its upside down star pyramid
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("   ");
+            }
+            for (int j = 1; j <= (2 * num) - (2 * i - 1); j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void pattern9(int num) {
+        for (int i = 1; i <= num; i++) {
+
             for (int j = 1; j <= num - i + 1; j++) {
                 System.out.print("   ");
             }
@@ -128,12 +152,88 @@ public class MustDoPatterns {
             for (int j = 1; j <= 2 * i - 1; j++) {
                 System.out.print(" * ");
             }
+            System.out.println();
+        }
 
-            for (int j = 1; j <= num - i + 1; j++) {
+        for (int i = 1; i <= num; i++) {
+
+            for (int j = 1; j <= i; j++) {
                 System.out.print("   ");
+            }
+
+            for (int j = 1; j <= 2 * num - (2 * i - 1); j++) {
+                System.out.print(" * ");
             }
             System.out.println();
         }
+    }
+
+    public void pattern10(int num) {
+
+        /*
+         * *
+         * *
+         * * *
+         * * * *
+         * * * * *
+         * * * *
+         * * *
+         * *
+         */
+        for (int i = 1; i <= 2 * num - 1; i++) {
+            int stars = i;
+
+            if (i > num) {
+                stars = 2 * num - i;
+            }
+            for (int j = 1; j <= stars; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public void pattern11(int num) {
+
+        int pat = 1;
+
+        for (int i = 1; i <= num; i++) {
+
+            if (i % 2 == 0) {
+                pat = 0;
+            } else {
+                pat = 1;
+            }
+
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print(pat);
+                pat = 1 - pat;
+            }
+            System.out.println();
+        }
+
+    }
+
+    public void pattern12(int num) {
+
+        for (int i = 1; i <= num; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+
+            for (int j = 1; j <= 2 * num - (2 * i - 1); j++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 1; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
     }
 
     public static void main(String[] args) {
@@ -142,12 +242,17 @@ public class MustDoPatterns {
 
         int num = 5;
 
-        //obj.pattern1(num);
-        //obj.pattern2(num);
-        //obj.pattern3(num);
-        //obj.pattern4(num);
-        //obj.pattern5(num);
-        //obj.pattern6(num);
-        obj.pattern7(num);
+        // obj.pattern1(num);
+        // obj.pattern2(num);
+        // obj.pattern3(num);
+        // obj.pattern4(num);
+        // obj.pattern5(num);
+        // obj.pattern6(num);
+        // obj.pattern7(num);
+        // obj.pattern8(num);
+        // obj.pattern9(num);
+        // obj.pattern10(num);
+        // obj.pattern11(num);
+        obj.pattern12(num);
     }
 }
